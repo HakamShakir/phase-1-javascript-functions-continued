924de1ab4a07b78825486baea769bd7bdce88590
HakamShakir <hakamshakir2002@gmail.com>
2022-08-10 16:33:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(task = "go to the office") {
+  return "This Monday, I will " + task + ".";
+}
+
+function wrapAdjective(a = "*") {
+  return function result(b) {
+    return "You are " + a + b + a + "!";
+  };
+}
